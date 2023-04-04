ff2a86d7045df99f7fdc9a90bc971589326f441d
ZachWinne <zach.winne@gmail.com>
2023-04-04 09:40:46 -0400

    add single constant for updating input area

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,26 +1,60 @@
 import React,{ useState } from "react";
 
 function CreateArea(props) {
-  const [titleText, setTitleText] = useState('')
-  const [contentText, setContentText] = useState('')
+  // const [titleText, setTitleText] = useState('')
+  // const [contentText, setContentText] = useState('')
 
-  function updateTitleText(event) {
-    setTitleText(event.target.value)
+
+  // Single constant to update note
+  const [note, setNote] = useState({
+    title: '',
+    content: ''
+  })
+
+
+  // function updateTitleText(event) {
+  //   setTitleText(event.target.value)
+  // }
+
+  // function updateContentText(event) {
+  //   setContentText(event.target.value)
+  // }
+
+  // Handling update of single constant note
+  function updateNote(event) {
+    const {name, value} = event.target;
+
+    setNote((prevValue) => {
+      return {
+        ...prevValue, 
+        [name]: value
+      }
+    })
   }
 
-  function updateContentText(event) {
-    setContentText(event.target.value)
-  }
+  // function addNote(event) {
+  //   props.setNoteList((prevList) => {
+  //     return [...prevList, {
+  //       noteTitle: titleText,
+  //       noteContent: contentText
+  //     }]
+  //   })
+  //   setTitleText('')
+  //   setContentText('')
+  //   event.preventDefault()
+  // }
 
   function addNote(event) {
     props.setNoteList((prevList) => {
       return [...prevList, {
-        noteTitle: titleText,
-        noteContent: contentText
+        noteTitle: note.title,
+        noteContent: note.content
       }]
     })
-    setTitleText('')
-    setContentText('')
+    setNote({
+      title: '',
+      content: ''
+    })
     event.preventDefault()
   }
 
@@ -28,17 +62,21 @@ function CreateArea(props) {
     <div>
       <form>
         <input 
-          onChange={updateTitleText} 
+          // onChange={updateTitleText} 
+          onChange={updateNote}
           name="title" 
           placeholder="Title" 
-          value={titleText}
+          // value={titleText}
+          value={note.title}
         />
         <textarea 
-          onChange={updateContentText}
+          // onChange={updateContentText}
+          onChange={updateNote}
           name="content" 
           placeholder="Take a note..." 
           rows="3" 
-          value={contentText}
+          // value={contentText}
+          value={note.content}
         />
         <button 
           onClick={addNote}
